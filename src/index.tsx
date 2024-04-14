9eb94608b257623487a8db64163c95acbc144b58
AlexandrTurov <alexandr.turov@mail.ru>
2024-04-15 02:15:44 +0700

    init project

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+// import App from './App.tsx'
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+function App() {
+  return (
+    <div>
+      App
+    </div>
+  );
+}
